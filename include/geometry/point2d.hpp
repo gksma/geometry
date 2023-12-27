17f4e3f79f154a4bf5b8ab788c4e089a2be6c74e
gksma <dhfz1794@naver.com>
2023-12-28 01:38:43 +0900

    Added basic description for point2d hpp

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file geometry/point2d.hpp
+ * @author HanHeum Cho (dhfz1794@naver.com)
+ * @brief Point class declaration with 2-dimension
+ * @version 1.0.0
+ * @date 2023-12-28
+ * @copyright Copyright (c) 2023 gksma, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 gksma, All Rights Reserved.
+// Authors: HanHeum Cho
+
+#ifndef GKSMA_GEOMETRY_POINT_2_HPP_
+#define GKSMA_GEOMETRY_POINT_2_HPP_
+
+#endif
