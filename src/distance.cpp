146aa9fbf5a348d6a1bd7bfc642ddd3ff3f40d17
gksma <dhfz1794@naver.com>
2024-01-10 19:48:21 +0900

    Added distance cpp file

---
+++ src/distance.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file distance.cpp
+ * @author gksma (dhfz1794@naver.com)
+ * @brief 
+ * @version 1.0.0
+ * @date 2024-01-10
+ * @copyright Copyright (c) 2024 gksma, All Rights Reserved.
+ */
