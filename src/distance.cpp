4748f9274f3bf55541f277a4a5f53731bf1d9c07
gksma <dhfz1794@naver.com>
2024-01-10 20:01:40 +0900

    Added cmakelists at cmakelists source files

--- src/distance.cpp
+++ src/distance.cpp
@@ -1,7 +1,7 @@
 /**
  * @file distance.cpp
  * @author gksma (dhfz1794@naver.com)
- * @brief 
+ * @brief Distance class implementation for 
  * @version 1.0.0
  * @date 2024-01-10
  * @copyright Copyright (c) 2024 gksma, All Rights Reserved.
